c60c273d8c1b8e5a2ffb377163cfa54aedf9ff97
杨国杰 <1152144009@qq.com>
2023-03-23 12:38:43 +0800

    修改配置

---
+++ src/view/chart.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/chart.scss";
+interface ChartProps {
+}
+const Chart: React.FC<ChartProps> = () => {
+    return (
+        <div className="chart">
+            <span>sdfsaf</span>
+        </div>
+    );
+};
+
+export default Chart;
